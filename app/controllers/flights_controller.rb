dee73749054afae04180beaf441935c7d7ba6483
patrickmontalto <pmontalto@gmail.com>
2015-11-23 17:37:05 -0500

    search functionality

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,9 @@
 class FlightsController < ApplicationController
   def index
     @airport_options = Airport.all.map{|a| [ a.code, a.id ] }
-    @date_options = Flight.all.map{|d| [d.departure_time.strftime("%D")]}.uniq.sort
+    @date_options = Flight.all.map{|d| [d.departure_time.strftime("%D"), d.departure_time]}.uniq {|d| d.first}.sort
+    date = params[:date].to_datetime.strftime("%F")
+    @flights = Flight.where(from_airport: params[:from_id]).where(
+                              to_airport: params[:to_id]).where('DATE(departure_time) = ?', date)
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,8 @@
 class Flight < ActiveRecord::Base
   belongs_to :from_airport, :class_name => "Airport", :foreign_key => "start_id"
   belongs_to :to_airport, :class_name => "Airport", :foreign_key => "finish_id"
+
+  def flight_date
+    self.departure_time.strftime("%D")
+  end
 end
